86e65687f95717ead7b51384f42466620deb2b46
Sophia Nguyen <sophia.nguyen@alumni.ubc.ca>
2019-06-22 00:32:13 -0700

    finished part 4: shopping list, local storage

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
-import { CSSTransition, TransitionGroup } from 'react-transition';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
 
 class ShoppingList extends Component {
@@ -14,14 +14,14 @@ class ShoppingList extends Component {
     }
 
     render() {
-        const { item } = this.state;
+        const { items } = this.state;
         return(
             <Container>
                 <Button
                     color="dark"
                     style={{marginBottom: '2rem'}}
                     onClick={() => {
-                        const name = promt('Enter Item');
+                        const name = prompt('Enter Item');
                         if (name) {
                             this.setState(state => ({
                                 items: [...state.items, { id: uuid(), name }]
@@ -29,7 +29,31 @@ class ShoppingList extends Component {
                         }
                     }}
                     >Add Item</Button>
+
+                    <ListGroup>
+                        <TransitionGroup className="shopping-list">
+                            {items.map(({ id, name }) => (
+                                <CSSTransition key={id} timeout={500} classNames="fade">
+                                    <ListGroupItem>
+                                        <Button
+                                            className="remove-btn"
+                                            color="danger"
+                                            size="sm"
+                                            onClick={() => {
+                                                this.setState(state => ({
+                                                    items: state.items.filter(item => item.id !== id)
+                                                }));
+                                            }}
+                                            >&times;</Button>
+                                        {name}
+                                    </ListGroupItem>
+                                </CSSTransition>
+                            ))}
+                        </TransitionGroup>
+                    </ListGroup>
             </Container>
         );
     }
 }
+
+export default ShoppingList;
